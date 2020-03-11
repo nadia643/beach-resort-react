f257f346a1310f586e4921ce301bada20a8c3404
John Doe <nadia643@gmail.com>
2020-03-11 13:33:24 +0000

    added services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Title from '../components/Title';
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa';
 
 
 export default class Services extends Component {
     render() {
         return (
             <div>
-                Hello from services
+                <Title title="services" />
             </div>
         )
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,20 @@ import React from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
+import Services from '../components/Services';
 
 
 export default function Home() {
-    return <Hero>
+    return (
+        <>
+    <Hero>
         <Banner title="Luxurious Rooms" subtitle="Deluxe Rooms starting at $299">
         <Link to='/rooms' className="btn-primary">
             Our rooms
             </Link>
             </Banner>
     </Hero>
+    <Services />
+    </>
+)
 }
