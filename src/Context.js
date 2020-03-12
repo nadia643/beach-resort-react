eac4be92302aadfd2f35d2bbbf996220365d5ee5
John Doe <nadia643@gmail.com>
2020-03-12 14:15:05 +0000

    adding data to context page

---
+++ src/Context.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import items from './data';
+
+const RoomContext = React.createContext();
+
+class RoomProvider extends Component {
+    state = {
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
+    };
+
+    //getData
+
+    componentDidMount() {
+        let rooms = this.formatData(items)
+    }
+
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id
+            let images = item.fields.images.map(image => image.fields.file.url);
+            let room = {...items.fields, images:images, id}
+            return room;
+        });
+        return tempItems
+    }
+
+    render() {
+        return (
+        <RoomContext.Provider value={{...this.state}}>
+            {this.props.children}
+        </RoomContext.Provider>
+        )
+    }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+
+export { RoomProvider, RoomConsumer, RoomContext };
