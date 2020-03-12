e9b4176fe114945e98dad27b0553a65499b94e0e
John Doe <nadia643@gmail.com>
2020-03-12 14:30:36 +0000

    added data till arrays are console logged

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,6 +4,9 @@ import { RoomContext } from '../Context';
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
+        const {FeaturedRooms: rooms} = this.context
+        console.log(rooms);
+        
         return (
             <div>
                 from featured rooms
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Title from '../components/Title';
-import {FaCocktail, FaHiking, FaShuttleVan, FaBeer, FaCentercode} from 'react-icons/fa';
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa';
 
 
 export default class Services extends Component {
