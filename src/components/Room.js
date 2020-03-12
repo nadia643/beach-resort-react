c3c2fd7cddf849b2a2b6a0c2c0b1114e5990a152
John Doe <nadia643@gmail.com>
2020-03-12 18:08:29 +0000

    featured room images showing

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default function Room() {
+export default function Room({room}) {
+    const {name, slug, images, price } = room;
+    
     return (
-        <div>
-            hello from room
-        </div>
+        <article className="room">
+            <div className="img-container">
+                <img src={images[0]} alt="single room" />
+            </div>
+        </article>
     )
 }
