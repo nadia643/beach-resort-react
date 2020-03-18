464a5c55f9f36f19b9ef014b5883cab18ccfb200
John Doe <nadia643@gmail.com>
2020-03-18 23:53:52 +0000

    accessing value in consumer wrapper

--- src/Context.js
+++ src/Context.js
@@ -57,7 +57,11 @@ class RoomProvider extends Component {
 const RoomConsumer = RoomContext.Consumer;
 
 export function withRoomConsumer(Component) {
-    return function
+    return function ConsumerWrapper(props) {
+        return <RoomConsumer>
+            {value => <Component {...props} context={value}/>} 
+        </RoomConsumer>
+    }
 }
 
 export { RoomProvider, RoomConsumer, RoomContext };
