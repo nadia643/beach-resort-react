4ed5c2941de21e45f0f024a593693ea3cac5786c
John Doe <nadia643@gmail.com>
2020-03-23 12:32:09 +0000

    added types to select box

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -13,6 +13,14 @@ export default function RoomsFilter({rooms}) {
         handleChange, type, capacity, price, minPrice, maxPrice,
         minSize, maxSize, breakfast, pets
     } = context;
+    //get unique types
+    let types = getUnique(rooms, 'type');
+    //add all
+    types = ['all', ...types];
+    //map to jsx
+    types = types.map((item, index) => {
+        return <option value={item} key={index}>{item}</option>
+    })
     return <section className="filter-container">
         <Title title="search-rooms" />
         <form className="filter-form">
@@ -26,6 +34,7 @@ export default function RoomsFilter({rooms}) {
                 className="form-control" 
                 onChange={handleChange}
                 >
+                    {types}
                 </select>
             </div>
             {/*end select type */}
